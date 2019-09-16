3cb12792914298c8a84b8d60a543aa3000be37dd
Lindquist <glindqu2@illinois.edu>
2019-09-15 22:08:14 -0500

    app can use api to query a player and display it in a template

--- mysite/sports/views.py
+++ mysite/sports/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#from django.template import loader
+import statsapi
 
 def index(request):
-	return HttpResponse("You're at the the sports index")
+	# return HttpResponse("You're at the the sports index")
+	x = statsapi.lookup_player('eloy jimenez')
+	context = {'player_name': x}
+	return render(request, 'sports/index.html', context)
+
+# def home(request):
+# 	#player = 
+# 	return render(request, 'templates/sports/home.html', {
+# 		'player_name': statsapi.lookup_player('eloy jimenez')
+# 	})
